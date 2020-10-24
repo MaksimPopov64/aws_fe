fa16ddbb2a88448d1f9fd484962018dba8a32200
Maksim_Popov <Maksim_Popov1@epam.com>
2020-10-24 23:14:33 +0400

    initialized serverless and app

---
+++ client/dist/precache-manifest.6d51a0514df057a952c2f27a585b8fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fa180c00c41c8546f378b154d0de49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb189e330089052b876",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee3323e1e279c2e92b48",
+    "url": "/static/js/2.f0a8cf8a.chunk.js"
+  },
+  {
+    "revision": "44d582a97d02337b347d751d7baba8fd",
+    "url": "/static/js/2.f0a8cf8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb189e330089052b876",
+    "url": "/static/js/main.ad2593a5.chunk.js"
+  },
+  {
+    "revision": "e8a10cf83e6f7ed42a3a",
+    "url": "/static/js/runtime-main.b9870059.js"
+  }
+]);
