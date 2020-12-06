37d4202a091f49aeebac54fc19047a1acc29f4be
Maksim_Popov <Maksim_Popov1@epam.com>
2020-12-06 21:39:27 +0400

    added eb endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   product: 'https://mmc7tf40ua.execute-api.eu-west-1.amazonaws.com/dev/product/',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://mmc7tf40ua.execute-api.eu-west-1.amazonaws.com/dev/products/',
-  bff: 'https://mmc7tf40ua.execute-api.eu-west-1.amazonaws.com/dev/products'
+  bff: 'https://mmc7tf40ua.execute-api.eu-west-1.amazonaws.com/dev/products',
+  cart: 'http://cart-api-dev2.us-west-2.elasticbeanstalk.com/'
 };
 
 export default API_PATHS;
